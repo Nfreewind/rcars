291e06849c2c676639d47881c6ef62fae8963b88
Jonas Buchli <buchlij@ethz.ch>
2018-03-03 15:48:03 +0000

    Added Licneses.

--- rcars_detector/src/rcars_detector_node.cpp
+++ rcars_detector/src/rcars_detector_node.cpp
@@ -1,3 +1,24 @@
+/*
+
+  This is the dectector ROS node of the RCARS RCARS (Robot-Centric Absolute Reference System) 
+    Copyright (C) 2018 - Michael Neunert, Michael Bloesch, Jonas Buchli, 
+    Agile and Dexterous Robotics Lab, ETH Zurich
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 #include <ros/ros.h>
 #include <ros/console.h>
 
